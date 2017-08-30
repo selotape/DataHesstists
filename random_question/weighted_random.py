c991c857d4ea61490631e8bd9c46137f12b9b0fc
Ron <ron@twiggle.com>
2017-08-30 19:50:20 +0300

    implement challenge for hess

---
+++ random_question/weighted_random.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+TEST_LOOP = 10000
+ACCURACY = 0.001
+
+sample_data = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+sample_weights = [0.1, 0.15, 0.4, 0.1, 0.05, 0.066666667, 0.133333333]
+
+
+
+# TODO: Hess should implement!
+def weighted_sampler(data, weights):
+    """Should return a random item from data array, weighted by the weights array"""
+    raise NotImplemented
+
+
+
+
+def close_enough(float1, float2):
+    return abs(float1 - float2) <= ACCURACY
+
+
+def main():
+    results_count = defaultdict(int)
+    for i in range(TEST_LOOP):
+        item = weighted_sampler(sample_data, sample_weights)
+        results_count[item] += 1
+    for item, weight in zip(sample_data, sample_weights):
+        assert close_enough(results_count[item] / TEST_LOOP, weight), "You suck :("
+    print('Great Success!!')
+
+
+if __name__ == '__main__':
+    main()
