cd282adb6b509ab1abd7708741235b1a93c01bab
admin <tomhess414@gmail.com>
2017-08-30 21:08:36 +0300

    3 experiment, i hope this time the master will approve

--- random_question/weighted_random.py
+++ random_question/weighted_random.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-
-TEST_LOOP = 10000
+import random
+TEST_LOOP = 1000000
 ACCURACY = 0.001
 
 sample_data = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
@@ -10,11 +10,26 @@ sample_weights = [0.1, 0.15, 0.4, 0.1, 0.05, 0.066666667, 0.133333333]
 
 # TODO: Hess should implement!
 def weighted_sampler(data, weights):
-    """Should return a random item from data array, weighted by the weights array"""
-    raise NotImplementedError("Hess should implement!")
+    random_number=random.random()
+    print('this is the:')
+    print(random_number)
+    Dfunction = arrange_array(weights)
+    print(Dfunction)
+    for x in range(len(Dfunction)):
+            if random_number >= Dfunction[x] and random_number <= Dfunction[x+1] :
+                print('this the data chosen' )
+                print(data[x])
+                return data[x]
 
 
 
+
+def arrange_array(weights):
+    tmp=list(weights)
+    for item in range(len(tmp)):
+        if item != 0:
+            tmp[item]=tmp[item] + tmp[item-1]
+    return [0] + tmp
 
 def close_enough(float1, float2):
     return abs(float1 - float2) <= ACCURACY
@@ -32,3 +47,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
